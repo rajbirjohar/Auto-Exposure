a3bcf8f8b6f7f8c563594be9cd9a0b1a92c00d18
r-jo <rdrsjck@gmail.com>
2021-05-01 09:38:31 -0700

    Built layout for each page

---
+++ components/Layout.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+export const siteTitle = "Auto Bots";
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta name="og:title" content={siteTitle} />
+        <meta name="description" content="Instagram but for cars." />
+      </Head>
+      <body>
+        <main className="flex flex-col w-full mx-auto md:mt-12 mt-4 p-6 text-md leading-7">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
