bbe5fa8276a2fcee10a09c74d0bc9ef3786e87f1
r-jo <rdrsjck+gh@gmail.com>
2021-05-10 16:45:37 -0700

    Added lazy load for post cards

--- components/Post.js
+++ components/Post.js
@@ -10,13 +10,18 @@ import Link from "next/link";
 export default function Post({ image, href, name, caption }) {
   return (
     <figure
-      className="bg-white flex flex-col flex-1 p-6 w-full sm:w-1/2 md:w-1/3 m-2 flex-1 shadow-lg hover:shadow-xl
+      className="bg-white flex flex-col flex-1 p-6 sm:min-w-1/2 md:min-w-1/3 m-2 shadow-lg hover:shadow-xl
       transition duration-300 ease-in-out rounded-lg"
     >
-      <div className="relative flex flex-col items-center w-80 h-80">
-        <Image src="/dummy.jpg" alt="Dummy Image" layout="fill" />
+      <div className="h-80 w-80 relative ring-2">
+        <Image
+          src="/dummy.jpg"
+          alt="Dummy Image"
+          layout="fill" // required
+          objectFit="contain" // change to suit your needs
+        />
       </div>
-      <div className="text-left">
+      <div className="text-left mt-6">
         <p className="font-bold text-xl">Lightning McQueen</p>
         <p>I am speed.</p>
       </div>
--- pages/feed.js
+++ pages/feed.js
@@ -1,5 +1,6 @@
-import Post from "../components/Post";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import PostLoad from "../components/PostLoad";
 
 export default function Feed() {
   return (
@@ -7,17 +8,17 @@ export default function Feed() {
       <section>
         <h1 className="font-bold text-6xl my-10 tracking-loose">Feed</h1>
         <div className="flex flex-wrap">
+          <PostLoad />
           <Post />
           <Post />
           <Post />
+          <PostLoad />
           <Post />
           <Post />
           <Post />
+          <PostLoad />
           <Post />
-          <Post />
-          <Post />
-          <Post />
-          <Post />
+          <PostLoad />
           <Post />
         </div>
       </section>
