abb73fef6b1217eb0f9b9af2e9d9aa9ee04aea8a
r-jo <rdrsjck+gh@gmail.com>
2021-05-10 16:09:05 -0700

    Added more posts and made it responsive

--- components/Post.js
+++ components/Post.js
@@ -9,7 +9,10 @@ import Link from "next/link";
 
 export default function Post({ image, href, name, caption }) {
   return (
-    <figure className="flex flex-col flex-1 p-6 shadow-lg hover:shadow-xl transition duration-300 ease-in-out rounded-lg">
+    <figure
+      className="bg-white flex flex-col flex-1 p-6 w-full sm:w-1/2 md:w-1/3 m-2 flex-1 shadow-lg hover:shadow-xl
+      transition duration-300 ease-in-out rounded-lg"
+    >
       <div className="relative flex flex-col items-center w-80 h-80">
         <Image src="/dummy.jpg" alt="Dummy Image" layout="fill" />
       </div>
--- pages/feed.js
+++ pages/feed.js
@@ -1,14 +1,26 @@
 import Post from "../components/Post";
+import Layout from "../components/Layout";
 
 export default function Feed() {
   return (
-    <section className="mx-auto max-w-7xl my-6">
-      <h1 className="font-bold text-2xl tracking-loose">Feed</h1>
-      <div className="flex space-x-6">
-        <Post />
-        <Post />
-        <Post />
-      </div>
-    </section>
+    <Layout>
+      <section>
+        <h1 className="font-bold text-6xl my-10 tracking-loose">Feed</h1>
+        <div className="flex flex-wrap">
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </div>
+      </section>
+    </Layout>
   );
 }
