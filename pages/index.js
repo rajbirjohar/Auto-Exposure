9fda10c3550322b336636d47c458b99792cdb751
isaac-18 <isaac.curiel6721@gmail.com>
2021-05-11 22:27:14 -0700

    Add basic hero image

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout main>
       <section className="mx-auto">
-        <div className="h-screen">
-          <h1>Home Page</h1>
-        </div>
-        <div className="h-screen">
-          <h1>Details</h1>
+        <div className="h-screen flex content-center items-center justify-center">
+          <div className="absolute top-0 h-full w-full bg-hero-image bg-center bg-cover"></div>
+          <div className="mx-auto">
+            <h1>Auto Exposure</h1>
+          </div>
         </div>
       </section>
     </Layout>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hero-image': "url('/sample-hero.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
