d6c15b69b9836666c78da16dac52b52ebff79522
r-jo <rdrsjck+gh@gmail.com>
2021-05-20 16:32:32 -0700

    Refactor

---
+++ db/post.js
@@ -0,0 +1,28 @@
+import { nanoid } from 'nanoid';
+
+export async function getPosts(db, from = new Date(), by, limit) {
+  return db
+    .collection('posts')
+    .find({
+      // Pagination: Fetch posts from before the input date or fetch from newest
+      ...(from && {
+        createdAt: {
+          $lte: from,
+        },
+      }),
+      ...(by && { creatorId: by }),
+    })
+    .sort({ createdAt: -1 })
+    .limit(limit || 10)
+    .toArray();
+}
+
+export async function insertPost(db, { caption, postPicture, creatorId }) {
+  return db.collection('posts').insertOne({
+    _id: nanoid(12),
+    caption,
+    creatorId,
+    postPicture,
+    createdAt: new Date(),
+  }).then(({ ops }) => ops[0]);
+}
